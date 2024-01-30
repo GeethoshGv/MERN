d5f82d46ace8cf2b77358dae77cc884ca1a5c418
GeethoshGv <geethoshgv.2236@gmail.com>
2024-01-31 00:50:57 +0530

    basic server setup

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,10 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  return res.status(200).send("<h1>zoro</h1>");
+});
+
 app.listen(PORT, (req, res) => {
   console.log(`server is running on port ${PORT}`);
 });
