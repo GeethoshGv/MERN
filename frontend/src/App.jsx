d0b0baeb859d7fe9a529924013b22a06a67a142d
GeethoshGv <geethoshgv.2236@gmail.com>
2024-01-31 18:22:12 +0530

    added react-router-dom for routing  and created pages folder for all the route pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Layout } from "./Layout";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Layout />
+    </>
+  );
 };
 
 export default App;
---
+++ frontend/src/Layout.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./style/main.scss";
+
+export const Layout = () => {
+  return <section className="layout">Layout</section>;
+};
