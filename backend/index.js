9646f29ce40461068c61e9adb6803db4e0ebb505
GeethoshGv <geethoshgv.2236@gmail.com>
2024-01-31 17:55:41 +0530

    initialize  Cross-Origin Resource Sharing

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,21 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import { mainProducts } from "./models/model.js";
 import productrouter from "./routes/ProductRoutes.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.get("/", (req, res) => {
   return res.status(200).send("<h1>zoro</h1>");
