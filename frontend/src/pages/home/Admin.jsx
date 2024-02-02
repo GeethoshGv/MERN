3bb71ed8068cf4b4b2147dda9d2e09a2ddf763f2
GeethoshGv <geethoshgv.2236@gmail.com>
2024-02-02 17:59:54 +0530

    Utilize the useEffect hook to fetch product data from http://localhost:5000/products

--- frontend/src/pages/home/Admin.jsx
+++ frontend/src/pages/home/Admin.jsx
@@ -1,11 +1,36 @@
 import React from "react";
 import "./admine.scss";
 import Loader from "../../components/Loader.jsx";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Admin = () => {
+  const [product, setProduct] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/products")
+      .then((res) => res.json())
+      .then((data) => setProduct(data));
+  }, []);
+
+  console.log(product);
+
   return (
-    <div className="admin_div">
-      <Loader />
+    <div className="main">
+      {product.length > 0 ? (
+        product.map((item) => {
+          return (
+            <div className="container" key={item._id}>
+              <h2>Category: {item.category}</h2>
+              <p>Brand: {item.brand}</p>
+              <p>Price: {item.price}</p>
+              <p>Description: {item.description}</p>
+              <p>Stock: {item.stock}</p>
+            </div>
+          );
+        })
+      ) : (
+        <Loader />
+      )}
     </div>
   );
 };
